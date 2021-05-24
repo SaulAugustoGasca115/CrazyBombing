83a65e138659c935a2b8b070c71e36ee63c472ce
Saul Augusto Gasca Farrera <sgfarrera115@hotmail.com>
2021-05-24 17:34:40 -0500

    Player Move script added

---
+++ AlphaCrazyBombing/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    [Header("Player Attributes")]
+    [SerializeField]
+    public float Speed = 8.0f;
+    public float rotationSpeed = 30.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float translation = Input.GetAxis("Vertical") * Speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+
+        transform.Translate(new Vector3(0,translation,0));
+        transform.Rotate(new Vector3(0,0,-rotation));
+
+    }
+
+}
